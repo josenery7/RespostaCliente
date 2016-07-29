d9d98ba1edf906535080621cc0b9647e8af0bc64
José Ângelo <joseangelodasilvanery@hotmail.com>
2016-07-29 19:31:08 -0300

    implementação do método get

--- RespostaCliente/src/servlet/HTMLServlet.java
+++ RespostaCliente/src/servlet/HTMLServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 public class HTMLServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = null;
+
+		try {
+			out = response.getWriter();
+
+			response.setContentType("text/html");
+
+			out.print("<html><body>");
+			out.print("<h1>Título da Página</h1>");
+			out.print("<strong>Este é um texto em negritos</strong>");
+			out.print("</body></html>");
+
+		} finally {
+			if(out != null) {
+				out.close();
+			}
+		}
 	}
 
 }
