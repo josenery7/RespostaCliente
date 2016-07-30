a83942b578e56efebf9eb03041daf452c2c837ef
José Ângelo <joseangelodasilvanery@hotmail.com>
2016-07-30 17:52:28 -0300

    implementação do metodo get no ArquivoServlet

--- RespostaCliente/src/servlet/ArquivoServlet.java
+++ RespostaCliente/src/servlet/ArquivoServlet.java
@@ -1,25 +1,49 @@
 package servlet;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ArquivoServlet
- */
 @WebServlet("/Arquivo")
 public class ArquivoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		OutputStream out = null;
+		FileInputStream fis = null;
+
+		try {
+			out = response.getOutputStream();
+			
+			response.addHeader("Content-Disposition", "attachment; filename=arquivo.zip");
+
+			response.setContentType("application/zip");
+
+			fis = new FileInputStream("/home/jose/Documentos/arquivo.zip");
+
+			byte[] buffer = new byte[1024];
+			int b;
+
+			while ((b = fis.read(buffer)) > -1) {
+				out.write(buffer, 0, b);
+			}
+
+		} finally {
+			if (fis != null) {
+				fis.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		}
 	}
 
 }
